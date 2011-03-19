ec4bbb79e2707882d9911cb2983b4c1ddf31c600
waylybaye <havelove@gmail.com>
2011-03-19 09:52:48 +0800

    可显示评论/设置页面

--- baye.php
+++ baye.php
@@ -10,43 +10,92 @@ Author URI: http://baye.wayly.net
 ?>
 
 <?php
+function bm_get_plugin_path($file){
+    $path = dirname($file);
+    $arr = preg_split('/plugins/', $path);
+    return $arr[1];
+    print_r('start-' . $arr[1]. '-end');
+}
+
 function bm_comments_open($open){
     return false;
 }
 function bm_comments_template($value){
 	return dirname(__FILE__) . '/comments.php';
 }
+function bm_install(){
+}
+function bm_remove(){
+}
 
 add_filter("comments_open", "bm_comments_open");
 add_filter("comments_template", "bm_comments_template");
 
+register_activation_hook(__FILE__, 'display_copyright_install');
+register_deactivation_hook(__FILE__, 'display_copyright_remove');
+
 function bm_admin_menu(){
-    add_options_page('BAYE.ME 管理页面', 'BAYE.ME', 'administrator', 'baye_me_admin', 'bm_admin');
+    add_options_page('BAYE.ME 设置', 'BAYE.ME', 'administrator', 'bm_settings', 'bm_settings');
+    add_submenu_page('edit-comments.php', 'BAYE.ME', 'BAYE.ME', 'moderate_comments', 'bm_admin', 'bm_admin');
 }
+
 function bm_admin(){
+    $api_key = get_option('bm_api_key');
+    $api_secret = get_option('bm_api_secret');
+    $token = md5($api_key . $api_secret);
+?>
+<script type="text/javascript">
+function show_tab(tab){
+    if( tab == 'settings' ){
+        iframe = document.getElementById('bm_iframe');
+        iframe.src = "http://baye.me/api/apis/";
+    }
+    if( tab == 'comments' ){
+        iframe = document.getElementById('bm_iframe');
+        iframe.src = "http://baye.me/wordpress/comments?apikey=<?php echo $api_key; ?>&token=<?php echo $token; ?>";
+    }
+}
+</script>
+<link rel="stylesheet" href='<?echo WP_CONTENT_URL . '/plugins' . bm_get_plugin_path(__FILE__); ?>/styles.css'/>
+<p id="bm_panel">
+<a class="button" href='javascript:show_tab("settings");'>API 设置</a>
+<a class="button" href='javascript:show_tab("comments");'>评论管理</a>
+<a class="button" href='javascript:show_tab("settings");'>评论审核</a>
+</p>
+<iframe id='bm_iframe' src="http://baye.me/wordpress/comments?apikey=<?php echo $api_key; ?>&token=<?php echo $token; ?>" width="100%" height="100%">
+</iframe>
+<?php
+}
+
+function bm_settings(){
+    $message = '设置已保存';
+    if( $_SERVER['REQUEST_METHOD']=='POST' ){
+        $api_key = $_POST['bm_api_key'];
+        $api_secret = $_POST['bm_api_secret'];
+        update_option('bm_api_key', $api_key);
+        update_option('bm_api_secret', $api_secret);
+        echo '<div class="updated"><p>'. $message .'</p></div>';
+    }
 ?>
 <div>
 <h2>设置</h2>
-    <form method="post" action="options.php">
+    <form method="post" action="">
     <?php /* 下面这行代码用来保存表单中内容到数据库 */ ?>
     <?php wp_nonce_field('update-options'); ?>
     <p>
+        <input type="hidden" name="update_option"/>
         <label >API KEY :</label>
-        <input type="text" name="bm_apikey" value="<?php echo get_option("bm_apikey"); ?>"></input>
+        <input style="width: 300px" type="text" name="bm_api_key" value="<?php echo get_option("bm_api_key"); ?>"></input>
     </p> 
     <p>
         <label >SECRET :</label>
-        <input type="text" name="bm_secret" ><?php echo get_option("bm_secret") ; ?></input>
+        <input style="width: 300px" type="text" name="bm_api_secret" value="<?php echo get_option("bm_api_secret"); ?>"></input>
     </p> 
-
     <p> 
-        <input type="hidden" name="action" value="update" /> 
-        <input type="hidden" name="page_options" value="display_copyright_text" /> 
         <input type="submit" value="保存设置" class="button-primary" /> 
     </p> 
     </form> 
 </div> 
-            
 <?php } 
 if( is_admin() ){
         add_action("admin_menu", "bm_admin_menu");
--- comments.php
+++ comments.php
@@ -1,7 +1,7 @@
 <div id="bm_comments">
 </div>
 <script type="text/javascript"> 
-    var bm_apikey = "51cb274a1f4311e0aaf30013776bb75a";
+    var bm_apikey = "<?php echo get_option("bm_api_key") ?>";
     var bm_identify = "entry_<?php echo get_the_ID(); ?>";
     var bm_url = "<?php echo get_permalink(); ?>";
     var bm_title = "<?php echo get_the_title(); ?>";
