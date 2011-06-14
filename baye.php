a6b5d6f4c9fb188d13dc6dda685e1d6117d8150d
waylybaye <waylybaye@wayly.net>
2011-06-14 09:00:46 +0800

    add seo options

--- baye.php
+++ baye.php
@@ -9,6 +9,32 @@ Author URI: http://baye.wayly.net
 */
 ?>
 <?php
+$BM_API_KEY = get_option('bm_api_key');
+$BM_API_SECRET = get_option('bm_api_secret');
+$BM_TOKEN = md5($BM_API_KEY . $BM_API_SECRET);
+
+function http_get($path, $params){
+    $fp = fsockopen("baye.me", 80, $errno, $errstr, 10) or exit($errstr."--->".$errno);         
+    
+    //$length = strlen($params);
+    //构造HTTP Header
+    $header = "GET $path?$params HTTP/1.1\r\n";         
+    $header .= "Host: baye.me\r\n";
+    $header .= "Content-Type: application/x-www-form-urlencoded\r\n";         
+    //$header .= "Content-Length: ".$length."\r\n";         
+    $header .= "Connection: Close\r\n\r\n";        
+    //添加GET Query
+    //$header .= $params."\r\n";
+    //发送GET 请求
+    //echo $header . "<br/";
+    fputs($fp, $header);         
+    while( !feof($fp) ){
+        $ret .= fgets($fp);
+    }
+    fclose($fp);
+    return substr($ret, strpos($ret, "\r\n\r\n") + 4 );
+    return $ret;
+}
 function bm_get_plugin_path($file){
     $path = dirname($file);
     $arr = preg_split('/plugins/', $path);
@@ -87,8 +113,14 @@ function bm_settings(){
     if( $_SERVER['REQUEST_METHOD']=='POST' ){
         $api_key = $_POST['bm_api_key'];
         $api_secret = $_POST['bm_api_secret'];
+        $seo_open = $_POST['bm_seo_open'];
         update_option('bm_api_key', $api_key);
         update_option('bm_api_secret', $api_secret);
+        if( $seo_open == "on" ){
+            update_option("bm_seo_open", "1");
+        }else{
+            update_option("bm_seo_open", "0");
+        }
         echo '<div class="updated"><p>'. $message .'</p></div>';
     }
 ?>
@@ -106,6 +138,12 @@ function bm_settings(){
         <label >SECRET :</label>
         <input style="width: 300px" type="text" name="bm_api_secret" value="<?php echo get_option("bm_api_secret"); ?>"></input>
     </p> 
+    <p>
+        <input id="seo_open" type="checkbox" name="bm_seo_open" <?php if( get_option("bm_seo_open") == '1'){ echo "checked"; }?>></input>
+        <label for="seo_open"><strong>开启SEO 优化</strong></label>
+        <p style="color:gray">开启SEO优化后，如果检测到是搜索引擎的爬虫则将在服务端渲染出评论，让爬虫能抓到评论内容<br/>
+        目前支持:百度，Google，搜狗，有道，雅虎，MSN，搜搜</p>
+    </p>
     <p> 
         <input type="submit" value="保存设置" class="button-primary" /> 
     </p> 
--- comments.php
+++ comments.php
@@ -1,12 +1,34 @@
-<div id="bm_comments">
-</div>
-<script type="text/javascript"> 
-    var bm_apikey = "<?php echo get_option("bm_api_key") ?>";
-    var bm_identify = "entry_<?php echo get_the_ID(); ?>";
-    var bm_url = "<?php echo get_permalink(); ?>";
-    var bm_title = "<?php echo get_the_title(); ?>";
-    var script = document.createElement('script');
-    script.type = "text/javascript";
-    script.src = "http://baye.me/embed.js?f=wordpress";
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
-</script> 
+<?php
+global $BM_API_KEY;
+global $BM_TOKEN;
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+$SPIDERS = array("YodaoBot", "Googlebot", "Yahoo!", "Baiduspider", "msnbot", "Sogou", "Soso");
+$is_spider = false;
+foreach( $SPIDERS as $spider ){
+    if( strpos($user_agent, $spider) > -1 ){
+        $is_spider = true;
+        break;
+    }
+}
+
+$seo_open = get_option("bm_seo_open");
+
+if( $seo_open == "1" && $is_spider ){
+    $params = "apikey=$BM_API_KEY&token=$BM_TOKEN&identify=entry_".get_the_ID();
+    echo http_get("/wordpress/load_comments", $params);
+}else{
+?>
+    <div id="bm_comments">
+    </div>
+    <script type="text/javascript"> 
+        var bm_apikey = "<?php echo get_option("bm_api_key") ?>";
+        var bm_identify = "entry_<?php echo get_the_ID(); ?>";
+        var bm_url = "<?php echo get_permalink(); ?>";
+        var bm_title = "<?php echo get_the_title(); ?>";
+        var script = document.createElement('script');
+        script.type = "text/javascript";
+        script.src = "http://baye.me/embed.js?f=wordpress";
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    </script> 
+
+<?php }  ?>
